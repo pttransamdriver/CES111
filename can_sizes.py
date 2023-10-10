40cb582561f54769f61d5eabc5113c582413e56f
Tim <entrepreneurracer@gmail.com>
2023-10-10 12:53:02 -0700

    Add can sizes list

---
+++ can_sizes.py
@@ -0,0 +1,26 @@
+"""
+File: fitness.py
+Author: Tim Illguth
+Professor: Christian Eisinger
+
+"""
+
+import math 
+
+#List stating the can sizes and costs 
+can_sizes = [
+    {"name": "#1 Picnic",  "radius": 6.83,  "height": 10.16, "cost": 0.28},
+    {"name": "#1 Tall",    "radius": 7.78,  "height": 11.91, "cost": 0.43},
+    {"name": "#2",         "radius": 8.73,  "height": 11.59, "cost": 0.45},
+    {"name": "#2.5",       "radius": 10.32, "height": 11.91, "cost": 0.61},
+    {"name": "#3 Cylinder","radius": 10.79, "height": 17.78, "cost": 0.86},
+    {"name": "#5",         "radius": 13.02, "height": 14.29, "cost": 0.83},
+    {"name": "#6Z",        "radius": 5.40,  "height": 8.89,  "cost": 0.22},
+    {"name": "#8Z",        "radius": 6.83,  "height": 7.62,  "cost": 0.26},
+    {"name": "#10",        "radius": 15.72, "height": 17.78, "cost": 1.53},
+    {"name": "#211",       "radius": 6.83,  "height": 12.38, "cost": 0.34},
+    {"name": "#300",       "radius": 7.62,  "height": 11.27, "cost": 0.38},
+    {"name": "#303",       "radius": 8.10,  "height": 11.11, "cost": 0.42},]
+
+
+
---
+++ testing.py
@@ -0,0 +1,5 @@
+import math
+
+def torus_volume(inner, outer):
+    """Compute and return the volume of a torus."""
+    vol = 2 * (math.pi * inner) ** 2 * outer
